d8b821df321e406a9cb6a4bb7873c8068aeba2f9
nguyenanhung <info@nguyenanhung.com>
2022-12-13 00:45:30 +0700

    Đang develop dở tính năng mobile_detect

--- helpers/mobile_detect.php
+++ helpers/mobile_detect.php
@@ -23,3 +23,29 @@ if (!function_exists('is_tablet')) {
         return $detect->isTablet($userAgent, $httpHeaders);
     }
 }
+if (!function_exists('is_smartphone')) {
+    function is_smartphone($userAgent = null, $httpHeaders = null)
+    {
+        return (is_mobile($userAgent, $httpHeaders) && !is_tablet($userAgent, $httpHeaders));
+    }
+}
+if (!function_exists('is_desktop')) {
+    function is_desktop($userAgent = null, $httpHeaders = null)
+    {
+        return (!is_mobile($userAgent, $httpHeaders) && !is_tablet($userAgent, $httpHeaders));
+    }
+}
+if (!function_exists('mobile_detect')) {
+    /**
+     * Function mobile_detect
+     *
+     * @return \Detection\MobileDetect
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 13/12/2022 44:28
+     */
+    function mobile_detect()
+    {
+        return new \Detection\MobileDetect();
+    }
+}
--- src/Environment.php
+++ src/Environment.php
@@ -19,8 +19,8 @@ namespace nguyenanhung\Libraries\Mobile;
  */
 interface Environment
 {
-    const VERSION       = '1.0.4';
-    const LAST_MODIFIED = '2022-12-07';
+    const VERSION       = '1.0.5';
+    const LAST_MODIFIED = '2022-12-13';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const AUTHOR_URL    = 'https://nguyenanhung.com';
